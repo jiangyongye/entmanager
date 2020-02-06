74dda9044bb122796d6408a99fee5a6f4f7912d6
Yiyoki <q429822178@yahoo.com>
2020-02-06 01:30:30 -0500

    修改注释 完善员工class

--- PersonalInfo.py
+++ PersonalInfo.py
@@ -1,7 +1,10 @@
+from random import random
+
+
 class PersonalInfo:
     def __init__(self, name, birth, country, sex, attitude, record, salary, company, workpoint):
         self.name = name
-        # birth格式为xxxx.xx（eg: 1995.03）
+        # birth格式为'xxxx.xx'（eg: 1995.03）
         self.birth = birth
         self.country = country
         self.sex = sex
@@ -13,17 +16,54 @@ class PersonalInfo:
         self.salary = salary
         self.company = company
         # 范围[0, 100]
-        self._workpoint = workpoint
+        self.__workpoint = workpoint
 
     def age(self, time):
-        return time - self.birth
+        # 此处time为年份即xxxx
+        return time - int(self.birth[0:4])
 
     def add_record(self, company, time):
-        # 自由身也算入company
+        # 此处time为原始数据即xxxx.xx
+        # 无公司(None)也看作是一个company
         # 更新上一个公司list的结束时间
         self.record[-1][2] = time
         # 创建新公司list的公司名和开始时间
         self.record.append([company, time, 'until now'])
 
+
+class Employee(PersonalInfo):
+    def __init__(self, name, birth, country, sex, attitude, record, salary, company, workpoint, ability={}):
+        PersonalInfo.__init__(self, name, birth, country, sex, attitude, record, salary, company, workpoint)
+        # 能力为一个list包含各项能力值的dict和等级评分(eg: {'领导力': 2, '训练': 1, 'level': 1})
+        self.ability = ability
+
+    def set_ability(self):
+        self.ability['SoundTeach'] = random.randint(0, 10)
+        self.ability['DanceTeach'] = random.randint(0, 10)
+        self.ability['ShapeTeach'] = random.randint(0, 10)
+        self.ability['ClothDesign'] = random.randint(0, 10)
+        self.ability['PR'] = random.randint(0, 10)
+        self.ability['Business'] = random.randint(0, 10)
+        self.ability['StarEyesight'] = random.randint(0, 10)
+
+    def set_level(self):
+        # 求总值
+        a_sum = 0
+        for ability in self.ability:
+            a_sum += self.ability[ability]
+        # 分类
+        level = a_sum//7
+        self.ability['level'] = level
+
+    def toward_attitude(self, person):
+        pass
+
+    def get_attitude(self, person):
+        pass
+
+
+class Trainee(PersonalInfo):
+    def __init__(self, name, birth, country, sex, attitude, record, salary, company, workpoint):
+        PersonalInfo.__init__(self, name, birth, country, sex, attitude, record, salary, company, workpoint)
 
 
